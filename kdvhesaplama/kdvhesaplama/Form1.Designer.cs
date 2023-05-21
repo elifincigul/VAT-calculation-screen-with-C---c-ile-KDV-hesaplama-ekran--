a1d361581574400fc2664aa4345062fcfdca5862
Elif Gül <elif.f389@gmail.com>
2023-05-21 17:59:19 +0300

    VAT calculation screen with C ( KDV hesaplama ekranı)
    screen that calculates the VAT of the entered fee. (girilen ücretin kdv'sini hesaplayan ekran)

---
+++ kdvhesaplama/kdvhesaplama/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace kdvhesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txturunadi = new System.Windows.Forms.TextBox();
+            this.txturunfiyati = new System.Windows.Forms.TextBox();
+            this.txturunkdv = new System.Windows.Forms.TextBox();
+            this.btnhesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ürün Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Ürün Fiyatı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(49, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "kdv";
+            // 
+            // txturunadi
+            // 
+            this.txturunadi.Location = new System.Drawing.Point(149, 46);
+            this.txturunadi.Name = "txturunadi";
+            this.txturunadi.Size = new System.Drawing.Size(100, 20);
+            this.txturunadi.TabIndex = 1;
+            // 
+            // txturunfiyati
+            // 
+            this.txturunfiyati.Location = new System.Drawing.Point(149, 81);
+            this.txturunfiyati.Name = "txturunfiyati";
+            this.txturunfiyati.Size = new System.Drawing.Size(100, 20);
+            this.txturunfiyati.TabIndex = 1;
+            // 
+            // txturunkdv
+            // 
+            this.txturunkdv.Location = new System.Drawing.Point(149, 107);
+            this.txturunkdv.Name = "txturunkdv";
+            this.txturunkdv.Size = new System.Drawing.Size(100, 20);
+            this.txturunkdv.TabIndex = 1;
+            // 
+            // btnhesapla
+            // 
+            this.btnhesapla.Location = new System.Drawing.Point(164, 168);
+            this.btnhesapla.Name = "btnhesapla";
+            this.btnhesapla.Size = new System.Drawing.Size(122, 69);
+            this.btnhesapla.TabIndex = 2;
+            this.btnhesapla.Text = "Hesapla";
+            this.btnhesapla.UseVisualStyleBackColor = true;
+            this.btnhesapla.Click += new System.EventHandler(this.btnhesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnhesapla);
+            this.Controls.Add(this.txturunkdv);
+            this.Controls.Add(this.txturunfiyati);
+            this.Controls.Add(this.txturunadi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txturunadi;
+        private System.Windows.Forms.TextBox txturunfiyati;
+        private System.Windows.Forms.TextBox txturunkdv;
+        private System.Windows.Forms.Button btnhesapla;
+    }
+}
+
---
+++ kdvhesaplama/kdvhesaplama/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kdvhesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnhesapla_Click(object sender, EventArgs e)
+        {
+            int label2, label3;
+            label2 = Convert.ToInt32(txturunfiyati.Text);
+            label3 = Convert.ToInt32(txturunkdv.Text);
+            int carpma;
+            carpma = Label2 * label3;
+            MessageBox.Show("hesapla="*carpma.ToString());
+
+            txturunadi.Clear();
+            txturunfiyati.Clear();
+            txturunkdv.Clear();
+        }
+    }
+}
